f204454c62cde198963d113a947a2a476117be39
tbingle <sandmanthomas9@icloud.com>
2025-01-30 13:16:26 -0500

    Task 1 - Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,11 @@
 //Inital Commit
+let purchaseAmount = 15;//Inital Price
+let discountPercentage = .1; //10% discount
+let finalAmount;
+if (purchaseAmount> 100){
+    finalAmount= purchaseAmount - (purchaseAmount*discountPercentage);
+}//10% Discount
+else{
+    finalAmount=purchaseAmount;
+}//No discount
+console.log(`Final amount after discount: $${finalAmount}`);//Log
