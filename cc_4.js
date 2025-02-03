707b271ab836b1e885c0462d7309fbed6f731e68
tbingle <sandmanthomas9@icloud.com>
2025-02-02 19:04:40 -0500

    Task 3 - Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -16,3 +16,8 @@ for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i];    
 }
 console.log(`Total Sales: $${totalSales}`);//Log
+let stock = 10;//Inital stock price
+while (stock > 0) {
+    console.log (`Stock Remaining: ${stock}`);
+    stock--;//Decrease stock to 0 
+}
